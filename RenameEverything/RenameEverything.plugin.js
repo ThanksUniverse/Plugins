63450e89e4becac5e6126ae1bcce53041a2bfb7a
Pedro <thanksuniverse333@outlook.com>
2022-06-10 08:15:03 -0300

    NOT Stable 0.0.3

--- RenameEverything/RenameEverything.plugin.js
+++ RenameEverything/RenameEverything.plugin.js
@@ -6,35 +6,37 @@
  * @description Change everything in the Discord client
  * @updateUrl https://raw.githubusercontent.com/ThanksUniverse/Plugins/dev/RenameEverything/RenameEverything.plugin.js?token=GHSAT0AAAAAABULNPNXFS4XYTRGBPIDVFV4YVC3FPQ
  */
-var c = 0
-var seconds = 0
-var minutes = 0
-var hours = 0
+var c = 0;
+var seconds = 0;
+var minutes = 0;
+var hours = 0;
 var executed = 0;
 var cooldown = 10000;
+var time = hours + ":" + minutes + ":" + seconds 
 
 module.exports = class RenameEverything {
 	load() {}
 	start() {
-      console.log('%c Starting Script... [' + cooldown / 1000 + ' Seconds]', "color: #ffaa00; font-size: 20px")
+		console.log("%c Starting Script... [" + cooldown / 1000 + " Seconds]", "color: #ffaa00; font-size: 20px");
 		globalThis.tempo = setInterval(renameEverything, cooldown);
 		//! TOTAL TIME
 		setInterval(function () {
-			seconds = seconds + 1
+			seconds = seconds + 1;
 
-         if ( seconds == 60) {
-            seconds = 0
-            minutes = minutes + 1
-         }
+			if (seconds == 60) {
+				seconds = 0;
+				minutes = minutes + 1;
+			}
 
-         if ( minutes == 60 ) {
-            minutes = 0
-            hours = hours + 1
-         }
-            
+			if (minutes == 60) {
+				minutes = 0;
+				hours = hours + 1;
+			}
+         time = hours + ":" + minutes + ":" + seconds 
 		}, 1000);
-
+      
 		function renameEverything() {
+         console.time("ExecutionTime: ");
 			executed = executed + 1;
 
 			//! Generate random number + than 1000
@@ -87,18 +89,24 @@ module.exports = class RenameEverything {
 			var newUser = document.querySelector(
 				"#app-mount > div.appDevToolsWrapper-1QxdQf > div > div.app-3xd6d0 > div > div.layers-OrUESM.layers-1YQhyW > div > div.container-1eFtFS > div > div > div.sidebar-1tnWFu > section > div.container-YkUktl.gameActivityToggleAdded-Yd-YxC > div.nameTag-sc-gpq.canCopy-IgTwyV > div.defaultColor-24IHKz.text-sm-normal-3Zj3Iv.usernameContainer-3PPkWq > div"
 			);
+
+			var newUserw = document.querySelector(
+				"#app-mount > div.appDevToolsWrapper-1QxdQf > div > div.app-3xd6d0 > div > div.layers-OrUESM.layers-1YQhyW > div > div.container-1eFtFS > div > div > div.sidebar-1tnWFu > section > div.container-YkUktl > div.nameTag-sc-gpq.canCopy-IgTwyV > div.text-xs-normal-3SiVjE.subtext-2HDqJ7"
+			);
+
 			var randomError = Math.floor(400 + Math.random() * 99);
 
-			if (newUser != null) {
+			if (newUser != null || newUserw != null) {
 				var oldUser = newUser.textContent;
 				newUser = newUser.innerHTML = "x = " + randomError;
 			} else {
-				newUser.textContent = "Loading...";
+				newUser = "Loading...";
+				newUserw = "Loading...";
 			}
 
 			if (randomError == 404) {
 				var text = "[Interval Time]:";
-				var css = "color: red";
+				var css = "color: red; font-size: 100px";
 				console.log("%c" + text, css, +cooldown);
 			}
 
@@ -117,88 +125,83 @@ module.exports = class RenameEverything {
 
 			//! Console all the changes made by the script
 
+         var GeneratedName = "[Generated Name]: " +            newUser +            " [Previous Name]: " +            oldUser 
+         var GeneratedID = ' \n%c [Generated ID]: " +            userID +            " [Previous ID]: " +            previousUserId'
+         
+         
+        /*  +            "\n%c It will be randomized again in: " +            "%c" +            cooldown / 1000 +            " seconds" +            "\n%c Executed: " +            executed +            " times ||" +            "|| Total Time: " +            seconds +            " seconds" */
+
+         var contentCss = "color: #faf; font-size: 18px;"
+
+         /* showToast(content, options = {}) */
+
 			if (minutes < 1) {
-            var results = console.log(
-				"%c [Generated Name]: " +
-					newUser +
-					" [Previous Name]: " +
-					oldUser +
-					"\n%c [Generated ID]: " +
-					userID +
-					" [Previous ID]: " +
-					previousUserId +
-					"\n%c It will be randomized again in: " +
-					"%c" +
-					cooldown / 1000 +
-					" seconds" +
-					"\n%c Executed: " +
-					executed +
-					" times ||" +
-					"|| Total Time: " +
-					seconds +
-					" seconds",
-				"color: #faf; font-size: 18px",
-				"color #f00; font-size: 18px",
-				"color: #afa; font-size: 18px",
-				"color: #aff; font-size: 18px",
-				"color: #badaea; font-size: 18px"
-			);
-         }
-          if ( minutes >= 1 ) {
-            results = null
-            var results2 = console.log(
-				"%c [Generated Name]: " +
-					newUser +
-					" [Previous Name]: " +
-					oldUser +
-					"\n%c [Generated ID]: " +
-					userID +
-					" [Previous ID]: " +
-					previousUserId +
-					"\n%c It will be randomized again in: " +
-					"%c" +
-					cooldown / 1000 +
-					" seconds" +
-					"\n%c Executed: " +
-					executed +
-					" times ||" +
-					"|| Total Time: " + minutes + " Minutes and " +
-					seconds +
-					" seconds",
-				"color: #faf; font-size: 18px",
-				"color #f00; font-size: 18px",
-				"color: #afa; font-size: 18px",
-				"color: #aff; font-size: 18px",
-				"color: #badaea; font-size: 18px"
-			);
-      }
-       if ( hours >= 1 ) {
-         results2 = null
-         var results3 = console.log(
-				"%c [Generated Name]: " +
-					newUser +
-					" [Previous Name]: " +
-					oldUser +
-					"\n%c [Generated ID]: " +
-					userID +
-					" [Previous ID]: " +
-					previousUserId +
-					"\n%c It will be randomized again in: " +
-					"%c" +
-					cooldown / 1000 +
-					" seconds" +
-					"\n%c [Executed: " +
-					executed +
-					" times ||" +
-					"|| Total Time: " + "Hours: " + hours + " Minutes: " + minutes + " Seconds: " +
-					seconds + "]",
-				"color: #faf; font-size: 18px",
-				"color #f00; font-size: 18px",
-				"color: #afa; font-size: 18px",
-				"color: #aff; font-size: 18px",
-				"color: #badaea; font-size: 18px"
-			);
-      }
+				var results = console.log(GeneratedName, contentCss +  GeneratedID);
+			}
+			if (minutes >= 1) {
+				results = null;
+				var results2 = console.log(
+					"%c [Generated Name]: " +
+						newUser +
+						" [Previous Name]: " +
+						oldUser +
+						"\n%c [Generated ID]: " +
+						userID +
+						" [Previous ID]: " +
+						previousUserId +
+						"\n%c It will be randomized again in: " +
+						"%c" +
+						cooldown / 1000 +
+						" seconds" +
+						"\n%c Executed: " +
+						executed +
+						" times ||" +
+						"|| Total Time: " +
+						minutes +
+						" Minutes and " +
+						seconds +
+						" seconds",
+					"color: #faf; font-size: 18px",
+					"color #f00; font-size: 18px",
+					"color: #afa; font-size: 18px",
+					"color: #aff; font-size: 18px",
+					"color: #badaea; font-size: 18px"
+				);
+			}
+			if (hours >= 1) {
+				results2 = null;
+				var results3 = console.log(
+					"%c [Generated Name]: " +
+						newUser +
+						" [Previous Name]: " +
+						oldUser +
+						"\n%c [Generated ID]: " +
+						userID +
+						" [Previous ID]: " +
+						previousUserId +
+						"\n%c It will be randomized again in: " +
+						"%c" +
+						cooldown / 1000 +
+						" seconds" +
+						"\n%c [Executed: " +
+						executed +
+						" times ||" +
+						"|| Total Time: " +
+						"Hours: " +
+						hours +
+						" Minutes: " +
+						minutes +
+						" Seconds: " +
+						seconds +
+						"]",
+					"color: #faf; font-size: 18px",
+					"color #f00; font-size: 18px",
+					"color: #afa; font-size: 18px",
+					"color: #aff; font-size: 18px",
+					"color: #badaea; font-size: 18px"
+				);
+			}
+         console.timeEnd("ExecutionTime: ");
 		}
 		function setGlobal() {
 			clearInterval(tempo);
@@ -207,9 +210,9 @@ module.exports = class RenameEverything {
 			tempo = setInterval(renameEverything, cooldown);
 		}
 	}
-   stop() {
-      console.log('%c Stopping Script... [It executed ' + executed + ' Times]', "color: #ffeaaa; font-size: 20px")
-      clearInterval(tempo);
-      return;
-   }
+	stop() {
+		clearInterval(tempo);
+		console.log("%c Stopping Script... [It executed " + executed + " Times]" + "\n%c Elapsed Time: " + time, "color: #ffeaaa; font-size: 20px", 'color: #00ffac; font-size: 18px');
+		return;
+	}
 };
